d841f9010b0d9bd91388e6b00cc3c955ce64ae78
optimum7 <optimum7@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-09 14:15:08 +0000

    git-svn-id: https://plugins.svn.wordpress.org/microdata-for-seo-by-optimum7com/trunk@461014 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/classes.php
@@ -0,0 +1,62 @@
+<?
+	class PostalAddress{
+		public $streetAddress;
+		public $addressLocality;
+		public $addressRegion;
+		public $postalCode;
+		public $id;		
+		public function __construct(){
+			$this->streetAddress='';
+			$this->addressLocality='';
+			$this->addressRegion='';
+			$this->id= rand();
+		}
+	}
+	
+	class AggregateOffer{
+		public $lowPrice;
+		public $highPrice;
+		public $id;
+		public function __construct(){
+			$this->lowPrice= '';
+			$this->id= rand();
+		}
+	}
+	
+	class AggregateRating{
+		public $reviewCount;
+		public $rating;
+		public $id;
+		public function __construct(){
+			$this->reviewCount= '';			
+			$this->rating= new Rating();
+			$this->id= rand();
+		}
+	}
+	
+	class Offer{
+		public $price;
+		public $availability;
+		public $aggregateOffer;
+		public $id;
+		public function __construct(){
+			$this->price= '';
+			$this->availability= '';
+			$this->aggregateOffer= new AggregateOffer;
+			$this->id= rand();
+		}
+	}
+	
+	class Rating{
+		public $ratingValue;
+		public $bestRating;
+		public $worstRating;
+		public $id;
+		public function __construct(){
+			$this->ratingValue= '';
+			$this->bestRating= '';
+			$this->worstRating= '';			
+			$this->id= rand();
+		}
+	}
+?>
---
+++ readme.txt
@@ -0,0 +1,17 @@
+=== Microdata for SEO by Optimum7.com ===
+Contributors: optimum7, jbeaujardin
+Tags: seo, google, microdata, html5, wordpress, ajax, jquery
+Requires at least: 3.0
+Tested up to: 3.2
+Stable tag: 2.5.3
+License: GPLv2 or later
+
+== Description ==
+Microdata for SEO by Optimum7.com allows you to add Microdata elements to your pages and posts on your blog.
+[Microdata for SEO](http://www.optimum7.com/internet-marketing/?utm_source=MicrodataPlugin)
+[Support and Questions](http://www.optimum7.com/contact-us?utm_source=MicrodataPlugin)
+This Plugin was built by [Optimum7](http://www.optimum7.com?utm_source=MicrodataPlugin)
+
+== Installation ==
+Upload the plugin to your blog and Activate it, that's set!
+1 & 2: You're done! There is no 3 at this time!
